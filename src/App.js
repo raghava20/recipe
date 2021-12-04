39afff8a7bec1b4ce4586cb808bbcb9bfb9b2e1e
raghava20 <raghavaanand22@gmail.com>
2021-12-04 08:54:25 +0530

    added css

--- src/App.js
+++ src/App.js
@@ -1,23 +1,39 @@
-import logo from './logo.svg';
 import './App.css';
 
 function App() {
+  // const recipe = "Chicken Tandoori";
+  const recipes = [
+    {
+      name: "Chicken Tandoori",
+      picture: "https://www.cubesnjuliennes.com/wp-content/uploads/2019/11/Tandoori-Chicken-1.jpg"
+    },
+    {
+      name: "Parotta Shawarma",
+      picture: "https://5.imimg.com/data5/NS/TO/YR/SELLER-5158094/shawarma-roll-500x500.jpeg"
+    },
+    {
+      name: "Chicken Briyani",
+      picture: "https://www.cubesnjuliennes.com/wp-content/uploads/2020/07/Chicken-Biryani-Recipe-500x500.jpg"
+    },
+    {
+      name: "Grill Chicken",
+      picture: "https://hips.hearstapps.com/hmg-prod/images/grilled-chicken-horizontal-1532030541.jpg"
+    },
+    {
+      name: "Gobi Manchurian",
+      picture: "https://www.honeywhatscooking.com/wp-content/uploads/2020/10/Baked-Gobi-Manchurian43.jpg"
+    }
+  ];
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <div className="recipe-list-container">
+        {recipes.map((recipe, index) => {
+          return (<div key={index} className="recipe-container">
+            <img className="recipe-picture" src={recipe.picture} alt={recipe.name} />
+            <p className="recipe-name">{recipe.name}</p>
+          </div>)
+        })}
+      </div>
     </div>
   );
 }
